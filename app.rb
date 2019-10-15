4075ac4b390ac9055285809174cadb7729410bb1
Cédric RENAUD <ruthhaba@gmail.com>
2019-10-15 23:45:46 +0200

    maj

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
-#require 'rubygems'
-#-------Pour le scrapping-------
-#require 'nokogiri'
-#require 'open-uri'
-#-------Pour simuler un navigateur-------
-#require 'watir'
-#require 'launchy'
-#-------Pour les tests------
-#require 'pry'
+#le programme rend toutes les gems disponibles dans tous les fichiers
+require 'bundler'
+Bundler.require
+
+#app.rb fait appel aux 2 autres fichiers présents dans \lib
+require_relative 'lib/game'
+require_relative 'lib/player'
